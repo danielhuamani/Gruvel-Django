9e834a0d7acd16d49a798f4ee5e3f01cd4ef7842
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2016-04-19 08:39:29 -0500

    agregando initial y model usuarios

---
+++ gruvel/util.py
@@ -0,0 +1,11 @@
+import json
+from os.path import join
+
+
+def get_env(base_dir):
+    """ Obtenemos las 'variables de entorno' desde un archivo json."""
+    with open(join(base_dir, 'gruvel/settings/settings.json')) as f:
+
+        env = json.load(f)
+        return env
+    return {}
